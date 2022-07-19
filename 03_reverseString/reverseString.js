29dd23390d56604853dba5c5af98775e2f5203a5
Stephen Smith <stephensmith750@gmail.com>
2022-07-19 19:33:58 +0100

    Complete javascript exercises 1 to 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string = '') {
+    let stringLength = string.length;
+    let stringArray = string.split('');
+    let reverseStringArray = [];
 
+    for (let i = 0; i < string.length; i++) {
+        reverseStringArray[i] = stringArray[stringLength - 1 - i];
+    }
+    let reversedString = reverseStringArray.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
